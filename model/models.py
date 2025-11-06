20d0d19d9d2237109f66a002ab66579a4a2b11fe
hanqing-shi <shq1847@gmail.com>
2025-11-05 19:34:21 -0800

    fix: wrong model reconstruction loss

--- model/models.py
+++ model/models.py
@@ -56,7 +56,7 @@ class GRUDecoder(nn.Module):
 
         # GRU input = [current_cond_t, prev_state]
         self.gru = nn.GRU(
-            input_size=cond_dim + out_dim + z_dim,
+            input_size=cond_dim + out_dim,
             hidden_size=hidden_dim,
             num_layers=num_layers,
             batch_first=True,
@@ -65,7 +65,7 @@ class GRUDecoder(nn.Module):
 
         self.out = nn.Linear(hidden_dim, out_dim)
 
-    def forward(self, cond_seq, z, x_future,teacher_force_ratio=0.0):
+    def forward(self, cond_seq, z, x_future,teacher_force_ratio):
         """
         Args:
             cond_seq: (B, T_pred, D_c)  - velocity or other condition sequence
@@ -82,13 +82,12 @@ class GRUDecoder(nn.Module):
         cond_vec = self.cond_enc(cond_seq)
         #cond_vec = cond_seq.mean(dim=1)  # (B, D_c)   # simple average pooling as condition summary
         h0 = self.init_mlp(torch.cat([cond_vec, z], dim=-1)).unsqueeze(0)  # (1,B,H)
-
         outputs = []
-        x_prev = x_future  # start from current state
-
-        for t in range(T_pred):
+        x_prev = x_future[:,0,:]  # x0
+        outputs = [x_prev.unsqueeze(1)]
+        for t in range(T_pred-1):
             cond_t = cond_seq[:, t, :]  # (B, D_c)
-            dec_in = torch.cat([cond_t, x_prev, z], dim=-1).unsqueeze(1)  # (B,1,D_c+D_x)
+            dec_in = torch.cat([cond_t,x_prev], dim=-1).unsqueeze(1)  # (B,1,D_c+D_x)
             dec_out, h0 = self.gru(dec_in, h0)                         # (B,1,H)
             x_pred = self.out(dec_out.squeeze(1))                      # (B,D_x)
             outputs.append(x_pred.unsqueeze(1))
@@ -122,8 +121,6 @@ class TrajCVAE(nn.Module):
     ):
         super().__init__()
         # Encoders
-        # TODO: we don't need to use GRU to encode condition
-        # self.cond_enc = GRUEncoder(cond_dim, cond_hidden, num_layers, bidir=bidir_cond, dropout=dropout)
         self.fut_enc  = GRUEncoder(traj_dim + cond_dim, fut_hidden,  num_layers, bidir=bidir_fut,  dropout=dropout)
         self.to_mu = nn.Linear(fut_hidden, z_dim)
         self.to_logvar = nn.Linear(fut_hidden, z_dim)
@@ -146,7 +143,7 @@ class TrajCVAE(nn.Module):
         return mu, logvar
 
     def decode(self, cond_seq, z, x0,teacher_force_ratio=1):
-        return self.decoder(cond_seq, z, x0,teacher_force_ratio=1)
+        return self.decoder(cond_seq, z, x0,teacher_force_ratio=teacher_force_ratio)
 
     def forward(self, x_future, cond_seq, beta=1.0, teacher_force_ratio=0.0):
         """
@@ -159,10 +156,8 @@ class TrajCVAE(nn.Module):
         z = reparameterize(mu, logvar)
 
         # use the first frame of ground truth trajectory as starting state
-
         y_hat = self.decode(cond_seq, z, x_future,teacher_force_ratio=teacher_force_ratio)
-
-        recon = F.mse_loss(y_hat, x_future, reduction="none").mean(dim=(1, 2))
+        recon = F.mse_loss(y_hat, x_future, reduction="none").sum(dim=(1, 2))
         kl = kl_divergence(mu, logvar)
         loss = (recon + beta * kl).mean()
         return y_hat, loss, {"recon": recon.mean().item(), "kl": kl.mean().item()}
@@ -194,93 +189,10 @@ class TrajCVAE(nn.Module):
         cond_seq_rep = cond_seq_rep.reshape(B * n_samples, T, -1)
 
         x0_rep = x0.unsqueeze(1).expand(B, n_samples, -1)
-        x0_rep = x0_rep.reshape(B * n_samples, -1)
-
+        x0_rep = x0_rep.reshape(B * n_samples, 1, -1)
+        # print('sample:',cond_seq_rep.shape, x0_rep.shape)
         # Sample from the standard normal prior.
         z = torch.randn(B * n_samples, z_dim, device=device)
 
         y_hat = self.decode(cond_seq_rep[:, :T_pred, :], z, x0_rep)
         return y_hat.view(B, n_samples, T_pred, -1)
-
-
-class PoseCVAE(nn.Module):
-    def __init__(
-        self,
-        seq_len: int,
-        d_pose: int,
-        d_condition: int,
-        latent_size: int,
-        feature_size: int,
-
-    ):
-        super.__init__()
-        
-        self.seq_len = seq_len
-        self.d_pose = d_pose
-        self.d_condition = d_condition
-        self.latent_size = latent_size
-        self.feature_size = feature_size
-
-        # encode
-        h1 = 256
-        # takes pose + condition as input
-        self.fc1 = nn.Linear(seq_len*(d_pose + d_condition),h1)
-        self.fc21 = nn.Linear(h1, latent_size)
-        self.fc22 = nn.Linear(h1, latent_size)
-
-        # decode
-        self.fc3 = nn.Linear(latent_size + d_condition, h1)
-        self.fc4 = nn.Linear(h1, feature_size)
-
-    def encode(self,pose_seq, cond_seq):
-        """
-        Args:
-            pose_seq: (B, T, D_pose)
-            cond_seq: (B, T, D_condition)
-        Returns:
-            mu, logvar
-        """
-        B, T, Dp = pose_seq.shape
-
-        x = torch.cat([pose_seq, cond_seq], dim=-1).reshape(B, T*(Dp + self.d_condition))
-        h1 = F.elu(self.fc1(x))
-        mu = self.fc21(h1)
-        logvar = self.fc22(h1)
-        return mu, logvar
-    
-    def decode(self, z, cond_seq):
-        """
-        Args:
-            z: (B, latent_size)
-            cond_seq: (B, T, D_condition)
-        Returns:
-            x_hat: (B, T, feature_size)
-        """
-        B, T, Dc = cond_seq.shape
-        # pad z to every frame
-        z_expand = z.unsqueeze(1).repeat(1,T,1) # (B, T, latent_size)
-        inputs = torch.cat([z_expand, cond_seq], dim=-1) # (B, T, latent_size + D_condition)
-
-        h = F.elu(self.fc3(inputs))
-        out = self.fc4(h) # (B, T, feature_size)
-        return out
-    
-    def forward(self, pose_seq, cond_seq, beta=1.0):
-        """
-        Args:
-            pose_seq: (B, T, D_pose)
-            cond_seq: (B, T, D_condition)
-        Returns:
-            x_hat: (B, T, feature_size)
-            mu, logvar
-        """
-        mu, logvar = self.encode(pose_seq, cond_seq)
-        z = reparameterize(mu, logvar)
-        x_hat = self.decode(z, cond_seq)
-
-        # loss
-        recon_loss = F.mse_loss(x_hat, pose_seq, reduction="mean")
-        kl_loss = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
-        loss = recon_loss + beta * kl_loss
-
-        return x_hat, loss, {"recon": recon_loss.item(), "kl": kl_loss.item()}
